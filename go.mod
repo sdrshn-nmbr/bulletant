41a2111fd69ed72b4aa544ad32a4b5044f6e948e
Sudarshan <sdrshnnmbr@gmail.com>
2024-07-29 21:21:36 -0500

    Actual initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sdrshn-nmbr/bulletant
+
+go 1.22.0
---
+++ internal/storage/storage.go
@@ -0,0 +1,21 @@
+package storage
+
+import (
+	"sync"
+	"time"
+)
+
+type Key []byte
+type Value []byte
+
+type Entry struct {
+	Key Key
+	Value Value
+	Timestamp time.Time
+}
+
+type Storage interface {
+	Get(key Key) (Value, error)
+	Put(key Key, value Value) error
+	Delete (key Key) error
+}
